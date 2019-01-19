c94f57e17bff2b5a1d64f4ec253ab49b3f373d6d
Eran Medagoda <eran.medagoda@gmail.com>
2019-01-19 17:18:55 +1000

    Added trained classifier for simulation traffic light data

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,9 +1,40 @@
 from styx_msgs.msg import TrafficLight
+import os
+import tensorflow as tf
+import numpy as np
+import rospy
 
 class TLClassifier(object):
     def __init__(self):
         #TODO load classifier
-        pass
+        
+        current_dir = os.path.dirname(os.path.realpath(__file__))
+        
+        #if sim:
+            model = current_dir + '/Trained_Models/simulation_model.pb'
+        #else:
+            #model = current_dir + '/Trained_Models/real_model.pb'
+        # end of if sim
+  
+        self.detection_graph = tf.Graph()
+    
+        with self.detection_graph.as_default():
+    
+            od_graph_def = tf.GraphDef()
+    
+            with tf.gfile.GFile(model, 'rb') as fid:
+    
+                serialized_graph = fid.read()
+                od_graph_def.ParseFromString(serialized_graph)
+                tf.import_graph_def(od_graph_def, name='')
+
+        self.session = tf.Session(graph=self.detection_graph)
+    
+        self.image_tensor = self.detection_graph.get_tensor_by_name('image_tensor:0')
+        self.detection_boxes = self.detection_graph.get_tensor_by_name('detection_boxes:0')
+        self.detection_scores = self.detection_graph.get_tensor_by_name('detection_scores:0')
+        self.detection_classes = self.detection_graph.get_tensor_by_name('detection_classes:0')
+        self.num_detections = self.detection_graph.get_tensor_by_name('num_detections:0')
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
@@ -16,4 +47,43 @@ class TLClassifier(object):
 
         """
         #TODO implement light color prediction
-        return TrafficLight.UNKNOWN
+        
+        image_expanded = np.expand_dims(image, axis=0)
+        
+        (boxes, scores, classes, num) = self.session.run([self.detection_boxes, 
+                                                              self.detection_scores, 
+                                                              self.detection_classes, 
+                                                              self.num_detections],
+                                                              feed_dict={self.image_tensor: image_expanded})
+    
+        boxes = np.squeeze(boxes)
+        classes = np.squeeze(classes).astype(np.int32)
+        scores = np.squeeze(scores)
+    
+        # Print class based on best score    
+        light_color = TrafficLight.UNKNOWN
+    
+        # find index with the max score[index]
+        max_score = scores[0]
+        max_index = 0
+        
+        for i in range(1, boxes.shape[0]):
+            if max_score < scores[i]:
+                max_score = scores[i]
+                max_index = i
+        
+        # determine which light state matches with the highest class score
+        if classes[max_index] == 1:
+            light_color = TrafficLight.GREEN
+        elif classes[max_index] == 2:
+            light_color = TrafficLight.RED
+        elif classes[max_index] == 3:
+            light_color = TrafficLight.YELLOW
+    
+        # if not sure of the light colour
+        if max_score < 0.5: # used to be 0.7
+            light_color = TrafficLight.UNKNOWN # return as unknown
+
+        return light_color
+        
+        #return TrafficLight.UNKNOWN
